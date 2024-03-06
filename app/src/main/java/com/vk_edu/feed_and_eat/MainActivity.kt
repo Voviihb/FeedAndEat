5fd33fd08542b5038fc45ac3e3f48b29de6254d0
Vladimir Bykov <Voviihb@gmail.com>
2024-03-06 22:34:09 +0300

    Work on Login Screen (3)

--- app/src/main/java/com/vk_edu/feed_and_eat/MainActivity.kt
+++ app/src/main/java/com/vk_edu/feed_and_eat/MainActivity.kt
@@ -3,13 +3,13 @@ package com.vk_edu.feed_and_eat
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.vk_edu.feed_and_eat.features.login.pres.LoginScreen
+import com.vk_edu.feed_and_eat.features.login.pres.RegisterScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            LoginScreen()
+            RegisterScreen()
         }
     }
 }
--- app/src/main/java/com/vk_edu/feed_and_eat/features/login/pres/LoginScreen.kt
+++ app/src/main/java/com/vk_edu/feed_and_eat/features/login/pres/LoginScreen.kt
@@ -18,6 +18,7 @@ import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.Icon
@@ -296,6 +297,320 @@ fun LoginScreen() {
                 )
         ) {
             Text(text = stringResource(R.string.sign_up_text), fontSize = 24.sp)
+        }
+    }
+}
+
+
+
+
+@Composable
+@OptIn(ExperimentalComposeUiApi::class)
+fun RegisterScreen() {
+    var email by remember { mutableStateOf("") }
+    var nickname by remember { mutableStateOf("") }
+    var password1 by remember { mutableStateOf("") }
+    var password2 by remember { mutableStateOf("") }
+
+    val (focusRequester) = FocusRequester.createRefs()
+    val keyboardController = LocalSoftwareKeyboardController.current
+    var passwordVisible by rememberSaveable { mutableStateOf(false) }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .paint(
+                painterResource(id = R.drawable.background_login),
+                contentScale = ContentScale.FillBounds
+            )
+    ) {
+        val roundValue = 12.dp
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                verticalAlignment = Alignment.Top,
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.logo_feed_and_eat),
+                    contentDescription = stringResource(R.string.feed_and_eat_logo),
+                    modifier = Modifier
+                        .size(width = 250.dp, height = 100.dp)
+                        .padding(top = 16.dp)
+                )
+            }
+
+            Column(
+                modifier = Modifier
+                    .sizeIn(minWidth = 300.dp)
+                    .align(Alignment.CenterHorizontally)
+            ) {
+                Text(
+                    text = stringResource(R.string.sign_up),
+                    modifier = Modifier
+                        .padding(top = 100.dp, bottom = 16.dp)
+                        .align(Alignment.Start),
+                    fontSize = 36.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.White
+                )
+            }
+
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+
+                /* Email text field */
+                Box(
+                    modifier = Modifier
+                        .width(300.dp)
+                        .padding(vertical = 8.dp)
+                        .clip(RoundedCornerShape(roundValue))
+                        .border(
+                            2.dp,
+                            colorResource(id = R.color.purple_fae),
+                            shape = RoundedCornerShape(roundValue)
+                        )
+
+                ) {
+                    TextField(
+                        value = email,
+                        onValueChange = { email = it },
+                        keyboardOptions = KeyboardOptions(
+                            keyboardType = KeyboardType.Email,
+                            imeAction = ImeAction.Next
+                        ),
+                        label = { Text(text = stringResource(R.string.enter_e_mail)) },
+                        leadingIcon = {
+                            Icon(
+                                Icons.Filled.Email,
+                                contentDescription = stringResource(R.string.email_logo)
+                            )
+                        },
+                        singleLine = true,
+                        modifier = Modifier.fillMaxWidth(),
+                        colors = TextFieldDefaults.colors(
+                            unfocusedContainerColor = Color.White,
+                            focusedContainerColor = Color.White,
+                            errorContainerColor = Color.White,
+                            focusedIndicatorColor = Color.Transparent,
+                            unfocusedIndicatorColor = Color.Transparent,
+                            disabledIndicatorColor = Color.Transparent,
+                            errorIndicatorColor = Color.Transparent,
+                            focusedTextColor = Color.Gray,
+                            unfocusedTextColor = Color.Gray,
+                            disabledTextColor = Color.Gray,
+                        )
+                    )
+                }
+
+                /* Nickname text field */
+                Box(
+                    modifier = Modifier
+                        .width(300.dp)
+                        .padding(vertical = 8.dp)
+                        .clip(RoundedCornerShape(roundValue))
+                        .border(
+                            2.dp,
+                            colorResource(id = R.color.purple_fae),
+                            shape = RoundedCornerShape(roundValue)
+                        )
+
+                ) {
+                    TextField(
+                        value = nickname,
+                        onValueChange = { nickname = it },
+                        keyboardOptions = KeyboardOptions(
+                            keyboardType = KeyboardType.Text,
+                            imeAction = ImeAction.Next
+                        ),
+                        label = { Text(stringResource(R.string.enter_nickname)) },
+                        leadingIcon = {
+                            Icon(
+                                Icons.Filled.Person,
+                                contentDescription = stringResource(R.string.person_logo)
+                            )
+                        },
+                        singleLine = true,
+                        modifier = Modifier.fillMaxWidth(),
+                        colors = TextFieldDefaults.colors(
+                            unfocusedContainerColor = Color.White,
+                            focusedContainerColor = Color.White,
+                            errorContainerColor = Color.White,
+                            focusedIndicatorColor = Color.Transparent,
+                            unfocusedIndicatorColor = Color.Transparent,
+                            disabledIndicatorColor = Color.Transparent,
+                            errorIndicatorColor = Color.Transparent,
+                            focusedTextColor = Color.Gray,
+                            unfocusedTextColor = Color.Gray,
+                            disabledTextColor = Color.Gray,
+                        )
+                    )
+                }
+
+                /* Password1 text field */
+                Box(
+                    modifier = Modifier
+                        .width(300.dp)
+                        .padding(vertical = 8.dp)
+                        .clip(RoundedCornerShape(roundValue))
+                        .border(
+                            2.dp,
+                            colorResource(id = R.color.purple_fae),
+                            shape = RoundedCornerShape(roundValue)
+                        )
+
+                ) {
+                    TextField(
+                        value = password1,
+                        onValueChange = { password1 = it },
+                        visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+                        keyboardOptions = KeyboardOptions(
+                            keyboardType = KeyboardType.Password,
+                            imeAction = ImeAction.Next
+                        ),
+                        label = { Text(stringResource(R.string.enter_password)) },
+                        leadingIcon = {
+                            Icon(
+                                Icons.Filled.Lock,
+                                contentDescription = stringResource(R.string.lock_logo)
+                            )
+                        },
+                        singleLine = true,
+                        modifier = Modifier.fillMaxWidth(),
+                        colors = TextFieldDefaults.colors(
+                            unfocusedContainerColor = Color.White,
+                            focusedContainerColor = Color.White,
+                            errorContainerColor = Color.White,
+                            focusedIndicatorColor = Color.Transparent,
+                            unfocusedIndicatorColor = Color.Transparent,
+                            disabledIndicatorColor = Color.Transparent,
+                            errorIndicatorColor = Color.Transparent,
+                            focusedTextColor = Color.Gray,
+                            unfocusedTextColor = Color.Gray,
+                            disabledTextColor = Color.Gray,
+                        )
+                    )
+                }
+
+                /* Password2 text field */
+                Box(
+                    modifier = Modifier
+                        .width(300.dp)
+                        .padding(vertical = 8.dp)
+                        .clip(RoundedCornerShape(roundValue))
+                        .border(
+                            2.dp,
+                            colorResource(id = R.color.purple_fae),
+                            shape = RoundedCornerShape(roundValue)
+                        )
+
+                ) {
+                    TextField(
+                        value = password2,
+                        onValueChange = { password2 = it },
+                        visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+                        keyboardOptions = KeyboardOptions(
+                            keyboardType = KeyboardType.Password,
+                            imeAction = ImeAction.Done
+                        ),
+                        keyboardActions = KeyboardActions(
+                            onDone = { keyboardController?.hide() }
+                        ),
+                        label = { Text(stringResource(R.string.repeat_password)) },
+                        leadingIcon = {
+                            Icon(
+                                Icons.Filled.Lock,
+                                contentDescription = stringResource(R.string.lock_logo)
+                            )
+                        },
+                        trailingIcon = {
+                            val image =
+                                if (passwordVisible) painterResource(id = R.drawable.hide_password_icon)
+                                else painterResource(id = R.drawable.show_password_icon)
+
+                            val description =
+                                if (passwordVisible) stringResource(R.string.hide_password) else stringResource(
+                                    R.string.show_password
+                                )
+
+                            IconButton(
+                                onClick = { passwordVisible = !passwordVisible },
+                                modifier = Modifier.size(24.dp)
+                            ) {
+                                Icon(image, description)
+                            }
+                        },
+                        singleLine = true,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .focusRequester(focusRequester),
+                        colors = TextFieldDefaults.colors(
+                            unfocusedContainerColor = Color.White,
+                            focusedContainerColor = Color.White,
+                            errorContainerColor = Color.White,
+                            focusedIndicatorColor = Color.Transparent,
+                            unfocusedIndicatorColor = Color.Transparent,
+                            disabledIndicatorColor = Color.Transparent,
+                            errorIndicatorColor = Color.Transparent,
+                            focusedTextColor = Color.Gray,
+                            unfocusedTextColor = Color.Gray,
+                            disabledTextColor = Color.Gray,
+                        )
+                    )
+                }
+
+                /* Sign up button */
+                Button(
+                    onClick = { /*TODO*/ },
+                    shape = RoundedCornerShape(roundValue),
+                    colors = ButtonColors(
+                        containerColor = colorResource(id = R.color.purple_fae),
+                        contentColor = colorResource(id = R.color.white),
+                        disabledContainerColor = colorResource(id = R.color.purple_fae),
+                        disabledContentColor = colorResource(id = R.color.white)
+                    ),
+                    modifier = Modifier
+                        .padding(vertical = 16.dp)
+                        .border(
+                            1.dp,
+                            colorResource(id = R.color.white),
+                            shape = RoundedCornerShape(roundValue)
+                        )
+                ) {
+                    Text(text = stringResource(R.string.sign_up), fontSize = 24.sp)
+                }
+
+            }
+        }
+
+        /* Log in button */
+        Button(
+            onClick = { /*TODO*/ },
+            shape = RoundedCornerShape(topStart = roundValue),
+            colors = ButtonColors(
+                containerColor = colorResource(id = R.color.purple_fae),
+                contentColor = colorResource(id = R.color.white),
+                disabledContainerColor = colorResource(id = R.color.purple_fae),
+                disabledContentColor = colorResource(id = R.color.white)
+            ),
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+                .border(
+                    1.dp,
+                    colorResource(id = R.color.white),
+                    shape = RoundedCornerShape(topStart = roundValue)
+                )
+        ) {
+            Text(text = stringResource(R.string.log_in), fontSize = 24.sp)
         }
     }
 }
