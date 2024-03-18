79c0519c4f44a8ef87f2a5901d675738989123dc
Vladimir Bykov <Voviihb@gmail.com>
2024-03-18 20:33:24 +0300

    refactoring

--- app/src/main/java/com/vk_edu/feed_and_eat/AuthProvider.kt
+++ app/src/main/java/com/vk_edu/feed_and_eat/AuthProvider.kt
@@ -11,7 +11,7 @@ import javax.inject.Singleton
 
 @Module
 @InstallIn (SingletonComponent::class)
-object MainModule {
+object AuthProvider {
     @Provides
     @Singleton
     fun provideFirebaseAuth(): FirebaseAuth {
