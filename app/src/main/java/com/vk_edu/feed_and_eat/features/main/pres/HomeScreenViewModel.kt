9ee21107435e99d4c3437b52df54be12e95326f0
Alex <mike123777mike@gmail.com>
2024-04-09 12:29:00 +0300

    fix 3.1 pull request

--- app/src/main/java/com/vk_edu/feed_and_eat/features/main/pres/HomeScreenViewModel.kt
+++ app/src/main/java/com/vk_edu/feed_and_eat/features/main/pres/HomeScreenViewModel.kt
@@ -2,7 +2,6 @@ package com.vk_edu.feed_and_eat.features.main.pres
 
 import androidx.compose.runtime.State
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.unit.Dp
