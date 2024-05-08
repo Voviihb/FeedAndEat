6c1d25218eb84d34caf5c3de5b9913ac9b8cb824
Vladimir Bykov <Voviihb@gmail.com>
2024-05-08 22:03:53 +0300

    make screen for review v1.0

--- app/src/main/java/com/vk_edu/feed_and_eat/features/recipe/pres/step/CongratulationsScreen.kt
+++ app/src/main/java/com/vk_edu/feed_and_eat/features/recipe/pres/step/CongratulationsScreen.kt
@@ -9,38 +9,51 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Text
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.vk_edu.feed_and_eat.R
+import com.vk_edu.feed_and_eat.common.graphics.RatingBar
+import com.vk_edu.feed_and_eat.features.dishes.domain.models.Review
 import com.vk_edu.feed_and_eat.features.navigation.pres.BottomScreen
+import com.vk_edu.feed_and_eat.ui.theme.LargeIconSize
 
 @Composable
 fun CongratulationScreen(
-    name : String,
-    navigateToRoute: (String) -> Unit
-){
+    name: String,
+    navigateToRoute: (String) -> Unit,
+    viewModel: CongratulationsScreenViewModel = hiltViewModel()
+) {
+    val review by viewModel.reviewState
     Column(
         verticalArrangement = Arrangement.SpaceBetween,
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier
             .fillMaxSize()
             .background(colorResource(id = R.color.white_cyan))
-    ){
+    ) {
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier
@@ -65,7 +78,7 @@ fun CongratulationScreen(
                 textAlign = TextAlign.Center
             )
             Spacer(modifier = Modifier.height(32.dp))
-            Box(modifier = Modifier.padding(8.dp)){
+            Box(modifier = Modifier.padding(8.dp)) {
                 Text(
                     text = name,
                     fontSize = 30.sp,
@@ -85,6 +98,9 @@ fun CongratulationScreen(
 
             Spacer(modifier = Modifier.height(60.dp))
         }
+
+        RateRecipe(review = review, viewModel = viewModel)
+
         Button(
             contentPadding = PaddingValues(0.dp),
             colors = ButtonDefaults.buttonColors(
@@ -105,5 +121,62 @@ fun CongratulationScreen(
                 overflow = TextOverflow.Visible
             )
         }
+    }
+}
+
+@Composable
+private fun RateRecipe(review: Review, viewModel: CongratulationsScreenViewModel) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = stringResource(R.string.rate_recipe),
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Normal
+        )
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(200.dp)
+                .padding(vertical = 8.dp)
+                .clip(RoundedCornerShape(24.dp))
+                .border(
+                    4.dp,
+                    colorResource(id = R.color.dark_cyan),
+                    shape = RoundedCornerShape(24.dp)
+                )
+                .background(Color.Transparent)
+
+        ) {
+            TextField(
+                value = review.message ?: "",
+                onValueChange = { viewModel.messageChanged(it) },
+                modifier = Modifier.fillMaxSize(),
+                colors = TextFieldDefaults.colors(
+                    unfocusedContainerColor = colorResource(id = R.color.white),
+                    focusedContainerColor = colorResource(id = R.color.white),
+                    errorContainerColor = colorResource(id = R.color.white),
+                    focusedIndicatorColor = Color.Transparent,
+                    unfocusedIndicatorColor = Color.Transparent,
+                    disabledIndicatorColor = Color.Transparent,
+                    focusedTextColor = Color.Black,
+                    unfocusedTextColor = Color.Black,
+                    disabledTextColor = Color.Black,
+                ),
+                placeholder = {
+                    Text(text = stringResource(R.string.enter_your_opinion_about_this_recipe))
+                },
+                textStyle = TextStyle(fontSize = 20.sp)
+            )
+        }
+        RatingBar(
+            review.mark.toFloat(),
+            stars = 5,
+            modifier = Modifier.height(LargeIconSize)
+        )
+
     }
 }
---
+++ app/src/main/java/com/vk_edu/feed_and_eat/features/recipe/pres/step/CongratulationsScreenViewModel.kt
@@ -0,0 +1,31 @@
+package com.vk_edu.feed_and_eat.features.recipe.pres.step
+
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import com.vk_edu.feed_and_eat.features.dishes.domain.models.Review
+import com.vk_edu.feed_and_eat.features.login.data.AuthRepoImpl
+import com.vk_edu.feed_and_eat.features.profile.data.UsersRepoImpl
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class CongratulationsScreenViewModel @Inject constructor(
+    private val _authRepo: AuthRepoImpl,
+    private val _usersRepo: UsersRepoImpl
+) : ViewModel() {
+    private val _reviewState = mutableStateOf(Review(0, 0.0, null))
+    val reviewState: State<Review> = _reviewState
+
+    fun markChanged(value: Double) {
+        _reviewState.value = _reviewState.value.copy(
+            mark = value
+        )
+    }
+
+    fun messageChanged(value: String) {
+        _reviewState.value = _reviewState.value.copy(
+            message = value
+        )
+    }
+}
