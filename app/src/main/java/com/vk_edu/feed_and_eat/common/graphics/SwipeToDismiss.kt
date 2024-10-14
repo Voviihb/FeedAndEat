359de590fbc66e9fbaac8d73e412df8844941938
Vladimir Bykov <Voviihb@gmail.com>
2024-10-14 18:55:20 +0300

    Add animation while deleting timer

--- app/src/main/java/com/vk_edu/feed_and_eat/common/graphics/SwipeToDismiss.kt
+++ app/src/main/java/com/vk_edu/feed_and_eat/common/graphics/SwipeToDismiss.kt
@@ -1,5 +1,9 @@
 package com.vk_edu.feed_and_eat.common.graphics
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.shrinkVertically
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,19 +15,34 @@ import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.SwipeToDismiss
 import androidx.compose.material.rememberDismissState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.vk_edu.feed_and_eat.R
+import kotlinx.coroutines.delay
 
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
-fun <T> SwipeToDismissItem(item: T, onDismissed: (T) -> Unit, content: @Composable (T) -> Unit) {
+fun <T> SwipeToDismissItem(
+    item: T,
+    onDismissed: (T) -> Unit,
+    content: @Composable (T) -> Unit,
+    animationDuration: Int = 300
+) {
+    var isRemoved by remember {
+        mutableStateOf(false)
+    }
     val state = rememberDismissState(
         confirmStateChange = { dismissValue ->
             if (dismissValue == DismissValue.DismissedToStart || dismissValue == DismissValue.DismissedToEnd) {
-                onDismissed(item)
+                isRemoved = true
                 true
             } else {
                 false
@@ -31,11 +50,26 @@ fun <T> SwipeToDismissItem(item: T, onDismissed: (T) -> Unit, content: @Composab
         }
     )
 
-    SwipeToDismiss(state = state,
-        directions = setOf(DismissDirection.StartToEnd, DismissDirection.EndToStart),
-        background = { DeleteBackground(swipeDismissState = state) },
-        dismissContent = { content(item) }
-    )
+    LaunchedEffect(key1 = isRemoved) {
+        if (isRemoved) {
+            delay(animationDuration.toLong())
+            onDismissed(item)
+        }
+    }
+
+    AnimatedVisibility(
+        visible = !isRemoved,
+        exit = shrinkVertically(
+            animationSpec = tween(durationMillis = animationDuration),
+            shrinkTowards = Alignment.Top
+        ) + fadeOut()
+    ) {
+        SwipeToDismiss(state = state,
+            directions = setOf(DismissDirection.StartToEnd, DismissDirection.EndToStart),
+            background = { DeleteBackground(swipeDismissState = state) },
+            dismissContent = { content(item) }
+        )
+    }
 }
 
 @OptIn(ExperimentalMaterialApi::class)
